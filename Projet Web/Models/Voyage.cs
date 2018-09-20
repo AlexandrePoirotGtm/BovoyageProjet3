368f380a456efd162998777e179a095b22881104
stephanecha <schamblain@gmail.com>
2018-09-20 10:06:51 +0200

    Classe Voyage

--- Projet Web/Models/Voyage.cs
+++ Projet Web/Models/Voyage.cs
@@ -7,5 +7,63 @@ namespace Projet_Web.Models
 {
     public class Voyage
     {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateAller { get; set; }
+
+        [Required]
+        public DateTime DateRetour { get; set; }
+
+        [Required]
+        public int PlacesDisponibles { get; set; }
+
+        [Required]
+        public decimal PrixParPersonne { get; set; }
+
+        public int IdDestination { get; set; }
+
+        [ForeignKey("IdDestination")]
+        public virtual Destination Destination { get; set; }
+
+        public int IdAgenceVoyage { get; set; }
+
+        [ForeignKey("IdAgenceVoyage")]
+        public virtual AgenceVoyage AgenceVoyage { get; set; }
+
+        //Implementation du constructeur par defaut nécéssaire à Entity
+        public Voyage() { }
+
+        public Voyage(DateTime dateAller, DateTime dateRetour, int placesDisponibles, decimal prixParPersonne,
+                      Destination destination, AgenceVoyage agenceVoyage)
+        {
+            DateAller = dateAller;
+            DateRetour = dateRetour;
+            PlacesDisponibles = placesDisponibles;
+            PrixParPersonne = prixParPersonne;
+            IdDestination = destination.Id;
+            IdAgenceVoyage = agenceVoyage.Id;
+        }
+
+        public Voyage(DateTime dateAller, DateTime dateRetour, int placesDisponibles, decimal prixParPersonne,
+                      int idDestination, int idAgenceVoyage)
+        {
+            DateAller = dateAller;
+            DateRetour = dateRetour;
+            PlacesDisponibles = placesDisponibles;
+            PrixParPersonne = prixParPersonne;
+            IdDestination = idDestination;
+            IdAgenceVoyage = idAgenceVoyage;
+        }
+
+        public void Reserver(int places)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Destination} du {DateAller.ToShortDateString()} au {DateRetour.ToShortDateString()}";
+        }
     }
 }
