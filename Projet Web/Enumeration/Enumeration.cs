395ea684fb8ffb8ca2c58a09ef468baa2e2fadf4
Alexandre <Volty0@orange.fr>
2018-09-20 09:46:40 +0200

    Squelette des models

---
+++ Projet Web/Enumeration/Enumeration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Web.Enumeration
+{
+    public class Enumeration
+    {
+        public enum Etat
+        {
+            EnAttente,
+            EnCours,
+            Refusee,
+            Acceptee
+        }
+
+        public enum TypeAssurance
+        {
+            Annulation
+        }
+
+        public enum RaisonAnnulationDossier
+        {
+            Client,
+            PlacesInsuffisantes
+        }
+    }
+}
---
+++ Projet Web/Models/AgenceVoyage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Web.Models
+{
+    public class AgenceVoyage
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+
+        
+       
+    }
+}
