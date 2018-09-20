a70b7d1bdeb03d2af003c7c964ea6982caba2ce3
jk <h>
2018-09-20 09:57:36 +0200

    Classe Destination

--- Projet Web/Models/Destination.cs
+++ Projet Web/Models/Destination.cs
@@ -7,5 +7,34 @@ namespace Projet_Web.Models
 {
     public class Destination
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Continent { get; set; }
+
+        [Required]
+        public string Pays { get; set; }
+
+        [Required]
+        public string Region { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual List<Voyage> Voyages { get; set; }
+
+        //Implementation du constructeur par defaut nécéssaire à Entity
+        public Destination() { }
+
+        public Destination(string continent, string pays, string region)
+        {
+            Continent = continent;
+            Pays = pays;
+            Region = region;
+        }
+
+        public override string ToString()
+        {
+            return $"{Pays} : {Region}";
+        }
     }
 }
