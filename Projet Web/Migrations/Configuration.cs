e6707bb756dd5ebc403e074e8be46c80fc0aa9dd
stephanecha <schamblain@gmail.com>
2018-09-20 10:45:12 +0200

    Ajout Configuration de la Migration

---
+++ Projet Web/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Projet_Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Web.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<BoVoyageDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
