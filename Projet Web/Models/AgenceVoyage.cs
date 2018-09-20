96586daf3fbdbc464a79432123ba576a804161c1
stephanecha <schamblain@gmail.com>
2018-09-20 10:10:10 +0200

    Classe AgenceVoyage

--- Projet Web/Models/AgenceVoyage.cs
+++ Projet Web/Models/AgenceVoyage.cs
@@ -7,10 +7,24 @@ namespace Projet_Web.Models
 {
     public class AgenceVoyage
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
+
+        [Required]
         public string Nom { get; set; }
 
-        
-       
+        public virtual List<Voyage> Voyages { get; set; }
+
+        //Implementation du constructeur par defaut nécéssaire à Entity
+        public AgenceVoyage() { }
+
+        public AgenceVoyage(string nom)
+        {
+            Nom = Nom;
+        }
+
+        public override string ToString()
+        {
+            return Nom;
+        }
     }
 }
